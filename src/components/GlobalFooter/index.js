a9ba52316b30cd7e457e4bddec6c0138c91290fb
lemonish <lemon_yp@hotmail.com>
2018-07-05 15:13:38 +0800

    登录页调整

--- src/components/GlobalFooter/index.js
+++ src/components/GlobalFooter/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import classNames from 'classnames';
 import styles from './index.less';
 
-const GlobalFooter = ({ className, links, copyright }) => {
+const GlobalFooter = ({ className, links, copyright, style }) => {
   const clsString = classNames(styles.globalFooter, className);
   return (
-    <div className={clsString}>
+    <div className={clsString} style={style}>
       {links && (
         <div className={styles.links}>
           {links.map(link => (
